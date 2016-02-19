de7f86ad991967ca0ce5b278e2941928a77bac6d
Adam Rackis <adamrackis@gmail.com>
2016-02-19 15:48:20 -0600

    bundle loader used manually

---
+++ dist/10.bundle.js
@@ -0,0 +1,23 @@
+webpackJsonp([10],{
+
+/***/ 11:
+/***/ function(module, exports) {
+
+	class SharedUtil1{
+	    constructor(){
+	        this.name = 'Shared Util 1'
+	    }
+	    sharedUtil1_Method1(){
+
+	    }
+	    sharedUtil2_Method1(){
+
+	    }
+	    sharedUtil3_Method1(){
+
+	    }
+	}
+
+/***/ }
+
+});
--- modules/Contacts/Contacts.js
+++ modules/Contacts/Contacts.js
@@ -1,5 +1,5 @@
 import 'script!./util/Global';
-import SharedUtil1 from '../../SharedUtilities/SharedUtility1';
+import SharedUtil1 from 'bundle!../../SharedUtilities/SharedUtility1';
 
 class Contacts{
     constructor(data){
